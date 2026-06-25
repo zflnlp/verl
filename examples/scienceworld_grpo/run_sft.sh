ce1763ffb40b60addd5e634a3237dda88c0e58ba
zflnlp <zfliangnlp@foxmail.com>
2026-06-25 18:05:02 +0800

    Revert run_sft.sh to single GPU version (remove FSDP)

--- examples/scienceworld_grpo/run_sft.sh
+++ examples/scienceworld_grpo/run_sft.sh
@@ -104,18 +104,6 @@ lr_scheduler_type: cosine
 warmup_ratio: 0.1
 fp16: true
 ddp_timeout: 180000000
-gradient_checkpointing: true
-
-### FSDP configuration
-fsdp: "full_shard auto_wrap"
-fsdp_config:
-  fsdp_offload_params: false
-  fsdp_backward_prefetch: "backward_pre"
-  fsdp_forward_prefetch: false
-  fsdp_use_orig_params: true
-  fsdp_cpu_ram_efficient_loading: true
-  fsdp_sync_module_states: true
-  fsdp_transformer_layer_cls_to_wrap: "Qwen2DecoderLayer"
 
 ### eval
 val_size: 0.1
